bb78198b204e0774c03249c2d9da9abf7642e6f4
himanshu14046@iiitd.ac.in <$#AZ_Mnemonic007>
2017-11-05 01:09:01 +0530

    MOd

--- src/2_SRC.py
+++ src/2_SRC.py
@@ -3,31 +3,43 @@ from sklearn import linear_model
 from sklearn.metrics import accuracy_score, mean_absolute_error
 import numpy as np
 
+import dataset_manager
 from lib import feature_convertor
 from lib.data_utils import convert_non_one_hot
 from src.algo.recon import NormalRecon
 
-X_train, y_train , X_test, y_test = feature_convertor.read_data_100k_gen_dataset("1");
 
-y_test = convert_non_one_hot(y_test  ,True);
+tot_mae = []
+tot_acc = []
 
-X_test = X_test[0 : 2000]
-y_test = y_test[0 : 2000]
 
-X_train = X_train[0 : 8000]
-y_train = y_train[0 : 8000]
+for i in ["1" , "2" , "3" , "4" , "5"]:
 
-print("Test Size", np.shape(y_test))
+    X_train, y_train , X_test, y_test = feature_convertor.read_data_100k_gen_dataset(i);
 
-clf = NormalRecon()
+    y_test = convert_non_one_hot(y_test  ,True);
 
-print(clf)
+    X_test = X_test[0 : 1000]
+    y_test = y_test[0 : 1000]
 
-clf.fit( X_train , y_train)
+    X_train = X_train[0 : 8000]
+    y_train = y_train[0 : 8000]
 
-y_pred = clf.predict(X_test);
+    print("Test Size", np.shape(y_test))
 
-print("Acc")
-print(accuracy_score(y_test , y_pred))
-print("MAE")
-print(mean_absolute_error(y_test , y_pred))
+    clf = NormalRecon()
+
+    print(clf)
+
+    clf.fit( X_train , y_train)
+
+    y_pred = clf.predict(X_test);
+
+    stats = "Acc" + str(accuracy_score(y_test, y_pred));
+    print(stats)
+
+    dataset_manager.dump_error("SRC" , stats)
+
+    stats = "MAE" + str(mean_absolute_error(y_test , y_pred))
+
+    dataset_manager.dump_error("SRC", stats)
--- src/3_KSRC.py
+++ src/3_KSRC.py
@@ -1,33 +1,41 @@
-
 from sklearn import linear_model
 from sklearn.metrics import accuracy_score, mean_absolute_error
 import numpy as np
 
+import dataset_manager
 from lib import feature_convertor
 from lib.data_utils import convert_non_one_hot
 from src.algo.kernel_recon import KernelRecon
 
-X_train, y_train , X_test, y_test = feature_convertor.read_data_100k_gen_dataset("1");
+tot_mae = []
+tot_acc = []
 
-y_test = convert_non_one_hot(y_test  ,True);
+for i in ["1", "2", "3", "4", "5"]:
+    X_train, y_train, X_test, y_test = feature_convertor.read_data_100k_gen_dataset(i);
 
-X_test = X_test[0 : 1000]
-y_test = y_test[0 : 1000]
+    y_test = convert_non_one_hot(y_test, True);
 
-X_train = X_train[0 : 10000]
-y_train = y_train[0 : 10000]
+    X_test = X_test[0: 1000]
+    y_test = y_test[0: 1000]
 
-print("Test Size", np.shape(y_test))
+    X_train = X_train[0: 8000]
+    y_train = y_train[0: 8000]
 
-clf = KernelRecon()
+    print("Test Size", np.shape(y_test))
 
-print(clf)
+    clf = KernelRecon()
 
-clf.fit( X_train , y_train)
+    print(clf)
 
-y_pred = clf.predict(X_test);
+    clf.fit(X_train, y_train)
 
-print("Acc")
-print(accuracy_score(y_test , y_pred))
-print("MAE")
-print(mean_absolute_error(y_test , y_pred))
+    y_pred = clf.predict(X_test);
+
+    stats = "Acc" + str(accuracy_score(y_test, y_pred));
+    print(stats)
+
+    dataset_manager.dump_error("KSRC", stats)
+
+    stats = "MAE" + str(mean_absolute_error(y_test, y_pred))
+
+    dataset_manager.dump_error("KSRC", stats)
