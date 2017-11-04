ca1264916d9b85de43f354ff683446d193df0135
Himanshu <himanshu14046@iiitd.ac.in>
2017-11-05 00:46:45 +0530

    Sec

--- src/2_SRC.py
+++ src/2_SRC.py
@@ -11,11 +11,11 @@ X_train, y_train , X_test, y_test = feature_convertor.read_data_100k_gen_dataset
 
 y_test = convert_non_one_hot(y_test  ,True);
 
-X_test = X_test[0 : 50]
-y_test = y_test[0 : 50]
+X_test = X_test[0 : 2000]
+y_test = y_test[0 : 2000]
 
-X_train = X_train[0 : 5000]
-y_train = y_train[0 : 5000]
+X_train = X_train[0 : 8000]
+y_train = y_train[0 : 8000]
 
 print("Test Size", np.shape(y_test))
 
